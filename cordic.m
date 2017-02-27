087e6b9c5d6ebbfb16343e75acc201690671f2ea
jtonete <jeutonete@gmail.com>
2017-02-27 15:52:53 -0300

    comments

--- cordic.m
+++ cordic.m
@@ -13,27 +13,27 @@ inpLUT = fi(atan(2 .^ (-((0:(niters-1))'))) .* (2/pi), z_NT); % Normalized
 AnGain = prod(sqrt(1+2.^(-2*(0:(niters-1))))); % CORDIC gain
 inv_An = 1 / AnGain; % 1/A_n inverse of CORDIC gain
 
-for idx = 1:length(theta)
-    % CORDIC rotation kernel iterations
-    [x_out(idx), y_out(idx), z_out(idx)] = ...
-        cordic_rot_kernel(...
-            fi(inv_An, xyNT), fi(0, xyNT), theta(idx), inpLUT, niters);
-end
+% for idx = 1:length(theta)
+    % % CORDIC rotation kernel iterations
+    % [x_out(idx), y_out(idx), z_out(idx)] = ...
+        % cordic_rot_kernel(...
+            % fi(inv_An, xyNT), fi(0, xyNT), theta(idx), inpLUT, niters);
+% end
 
-% Plot the CORDIC-approximated sine and cosine values
-figure;
-subplot(411);
-plot(thNorm, x_out);
-axis([-1 1 -1 1]);
-title('Normalized X Values from CORDIC Rotation Kernel Iterations');
-subplot(412);
-thetaRadians = pi/2 .* thNorm; % real-world range [-pi/2 pi/2] angle values
-plot(thNorm, cos(thetaRadians) - double(x_out));
-title('Error between MATLAB COS Reference Values and X Values');
-subplot(413);
-plot(thNorm, y_out);
-axis([-1 1 -1 1]);
-title('Normalized Y Values from CORDIC Rotation Kernel Iterations');
-subplot(414);
-plot(thNorm, sin(thetaRadians) - double(y_out));
-title('Error between MATLAB SIN Reference Values and Y Values');
+% % Plot the CORDIC-approximated sine and cosine values
+% figure;
+% subplot(411);
+% plot(thNorm, x_out);
+% axis([-1 1 -1 1]);
+% title('Normalized X Values from CORDIC Rotation Kernel Iterations');
+% subplot(412);
+% thetaRadians = pi/2 .* thNorm; % real-world range [-pi/2 pi/2] angle values
+% plot(thNorm, cos(thetaRadians) - double(x_out));
+% title('Error between MATLAB COS Reference Values and X Values');
+% subplot(413);
+% plot(thNorm, y_out);
+% axis([-1 1 -1 1]);
+% title('Normalized Y Values from CORDIC Rotation Kernel Iterations');
+% subplot(414);
+% plot(thNorm, sin(thetaRadians) - double(y_out));
+% title('Error between MATLAB SIN Reference Values and Y Values');
